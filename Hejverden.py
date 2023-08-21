7fe79e9cbf0497335d95aed1c562fb1e77b65db2
Gustav <gust4219@edu.nextkbh.dk>
2023-08-21 14:56:13 +0200

    Changes to be committed: 	modified:   Hejverden.py

--- Hejverden.py
+++ Hejverden.py
@@ -1,1 +1,24 @@
-print("Hej verden")
+print("Opgave 1")
+
+print("Hej med dig. Jeg elsker at kode")
+
+print("Opgave 2")
+
+r=input("skriv radius:")
+r=float(r)
+
+print("Radius =",r)
+
+pi=3.14
+
+A= pi*(r*r)
+
+O= pi*(r*2)
+
+print("Areal =",A)
+print("Omkreds =",O)
+
+print("Opgave 3")
+
+
+
