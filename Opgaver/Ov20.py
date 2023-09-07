07fa2978f71400ed6582f5b32b5c9af6e6249d44
Gustav <gust4219@edu.nextkbh.dk>
2023-09-07 14:58:30 +0200

    Changes to be committed: 	modified:   Opgaver/Ov20.py 	new file:   "Tr\303\246ning/test.py"

--- Opgaver/Ov20.py
+++ Opgaver/Ov20.py
@@ -1,2 +1,11 @@
 print("Opgave 20")
       
+#n=int(input("Skriv startantallet af stjerner: "))
+
+n = 10
+
+for k in range(n):
+    s=""
+    for j in range(n-k):
+        s += "*"
+    print(s)
---
+++ Træning/test.py
@@ -0,0 +1,10 @@
+karakterer = [10,10,12,10,10,7,10,12,12,0]
+
+s=0
+
+for k in karakterer:
+    s+=k
+
+    print (s)
+   
+    
